16e3ed18494aeb0a461bff317df47253a981df30
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-14 15:15:19 +0500

    Value prop

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -2,8 +2,10 @@ import { createContext, useContext } from "react";
 
 const BlogContext = createContext();
 
+
+
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return <BlogContext.Provider value={{}}>{children}</BlogContext.Provider>;
 };
 
 // export default useBlogs = () => {
